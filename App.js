1c33179ead8efc5e977618daccced44c4f062d23
RAHUL TANWAR <rahul@magicapps.co.in>
2022-01-04 14:04:22 +0530

    React Navigation

--- App.js
+++ App.js
@@ -1,27 +1,22 @@
-// In App.js in a new project
-
 import * as React from 'react';
-import {View, Text,Button} from 'react-native';
+import {View, Text, Button} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
- 
-
 const Stack = createNativeStackNavigator();
-
 function App() {
   return (
     <NavigationContainer>
-        <Stack.Navigator initialRouteName="Home">
+      <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Details" component={DetailsScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
-function HomeScreen({ navigation }) {
+function HomeScreen({navigation}) {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       <Text>Home Screen</Text>
       <Button
         title="Go to Details"
@@ -31,10 +26,9 @@ function HomeScreen({ navigation }) {
   );
 }
 
-
-function DetailsScreen({ navigation }) {
+function DetailsScreen({navigation}) {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       <Text>Details Screen</Text>
       <Button
         title="Go to Details... again"
